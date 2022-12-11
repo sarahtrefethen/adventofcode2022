4e28438512d53d93ebc86b39fea0606970e7ef5c
Sarah Trefethen <49411879+sarahtrefethen@users.noreply.github.com>
2022-12-11 18:43:46 -0500

    day 11 part 2

--- D11.py
+++ D11.py
@@ -1,37 +1,78 @@
 
 import math
+from functools import reduce
 
-monkeys = [{} for i in range(8)]
+def part1():
+    monkeys = [{} for i in range(8)]
 
-def parse_monkey(index: int,raw_monkey: str):
-    [starting_line, operation_line, test_line, true_line, false_line] = range(1, 6)
-    lines = raw_monkey.split('\n')
-    monkey = monkeys[index]
+    def parse_monkey(index: int,raw_monkey: str):
+        [starting_line, operation_line, test_line, true_line, false_line] = range(1, 6)
+        lines = raw_monkey.split('\n')
+        monkey = monkeys[index]
 
-    monkey['items'] = [int(num) for num in lines[starting_line].split(": ")[1].split(",")]
-    monkey['inspect'] = lines[operation_line].split("= ")[1]
-    test_int = int(lines[test_line].split(" ")[-1])
-    true_int = int(lines[true_line].split(" ")[-1])
-    false_int = int(lines[false_line].split(" ")[-1])
-    monkey['test'] = lambda item: true_int if item % test_int == 0 else false_int
-    monkey['counter'] = 0
+        monkey['items'] = [int(num) for num in lines[starting_line].split(": ")[1].split(",")]
+        monkey['inspect'] = lines[operation_line].split("= ")[1]
+        test_int = int(lines[test_line].split(" ")[-1])
+        true_int = int(lines[true_line].split(" ")[-1])
+        false_int = int(lines[false_line].split(" ")[-1])
+        monkey['test'] = lambda item: true_int if item % test_int == 0 else false_int
+        monkey['counter'] = 0
 
-with open("D11input.txt") as input:
-    monkey_index = 0
-    raw_monkies = input.read().split("\n\n")
-    monkies = [parse_monkey(index, monkey) for index, monkey in enumerate(raw_monkies)]
-    
+    with open("D11input.txt") as input:
+        monkey_index = 0
+        raw_monkies = input.read().split("\n\n")
+        [parse_monkey(index, monkey) for index, monkey in enumerate(raw_monkies)]
+        
 
-for n in range(20):
-    for monkey in monkeys:
-        for old in monkey['items']:
-            monkey['counter'] += 1
-            new_worry = math.floor(eval(monkey['inspect']) / 3)
-            new_monkey = monkey['test'](new_worry)
-            monkeys[new_monkey]['items'].append(new_worry)
-        monkey['items'] = []
+    for n in range(20):
+        for monkey in monkeys:
+            for old in monkey['items']:
+                monkey['counter'] += 1
+                new_worry = math.floor(eval(monkey['inspect']) / 3)
+                new_monkey = monkey['test'](new_worry)
+                monkeys[new_monkey]['items'].append(new_worry)
+            monkey['items'] = []
 
 
-counters = [monkey['counter'] for monkey in monkeys]
-counters.sort()
-print(counters[-2] * counters[-1])
+    counters = [monkey['counter'] for monkey in monkeys]
+    counters.sort()
+    print(counters[-2] * counters[-1])
+
+
+def part2():
+    monkeys = [{} for i in range(8)]
+
+    def parse_monkey(index: int,raw_monkey: str):
+        [starting_line, operation_line, test_line, true_line, false_line] = range(1, 6)
+        lines = raw_monkey.split('\n')
+        monkey = monkeys[index]
+
+        monkey['items'] = [int(num) for num in lines[starting_line].split(": ")[1].split(",")]
+        monkey['inspect'] = lines[operation_line].split("= ")[1]
+        test_int = int(lines[test_line].split(" ")[-1])
+        true_int = int(lines[true_line].split(" ")[-1])
+        false_int = int(lines[false_line].split(" ")[-1])
+        monkey['test'] = lambda item: true_int if item % test_int == 0 else false_int
+        monkey['counter'] = 0
+        monkey['test_int'] = test_int
+
+    with open("D11input.txt") as input:
+        monkey_index = 0
+        raw_monkies = input.read().split("\n\n")
+        [parse_monkey(index, monkey) for index, monkey in enumerate(raw_monkies)]
+
+    magic_monkey_multiplier = reduce(lambda a, b: a*b, [monkey["test_int"] for monkey in monkeys])
+
+    for n in range(10000):
+        for monkey in monkeys:
+            for old in monkey['items']:
+                monkey['counter'] += 1
+                new_worry = eval(monkey['inspect']) % magic_monkey_multiplier
+                new_monkey = monkey['test'](new_worry)
+                monkeys[new_monkey]['items'].append(new_worry)
+            monkey['items'] = []
+
+
+    counters = [monkey['counter'] for monkey in monkeys]
+    counters.sort()
+    print(counters[-2] * counters[-1])
