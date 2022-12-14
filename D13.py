88273ff49e45806d7fe41a0d0108805a168360de
Sarah Trefethen <49411879+sarahtrefethen@users.noreply.github.com>
2022-12-13 20:22:31 -0500

    day 13 part 2

--- D13.py
+++ D13.py
@@ -1,15 +1,11 @@
-
-
-with open("D13input.txt") as input:
-    text = input.read()
-    pairs = [(eval(x), eval(y)) for [x, y] in [pair.split('\n') for pair in text.split('\n\n')]]
+from functools import cmp_to_key
 
 def compare_items(l, r):
     if isinstance(l, int) and isinstance(r, int):
         if l == r:
-            return "continue"
+            return 0
         else:
-            return "yes" if l < r else "no"    
+            return 1 if l < r else -1   
     if isinstance(l, list) and isinstance(r, list):
         return compare_lists(l, r)
     elif isinstance(l, int):
@@ -20,37 +16,45 @@ def compare_items(l, r):
 
 def compare_lists(left, right):
     if left == right:
-        return 'continue'
+        return 0
     elif left == []:
-        return 'yes'
+        return 1
     elif right == []:
-        return 'no'
+        return -1
 
-
-    response = "continue"
+    response = 0
     idx = 0
-    while response == "continue":
-
+    while response == 0:
         if idx == len(left) == len(right):
-            return 'continue'
+            return 0
         if idx == len(left):
-            return 'yes'
+            return 1
         if idx == len(right):
-            return 'no'
+            return -1
         response = compare_items(left[idx], right[idx])
         idx += 1
 
     return response
 
 
-sum = 0
-for index, (left, right) in enumerate(pairs):
-    print(left)
-    print(right)
-    if compare_lists(left, right) == 'yes':
-        sum += index+1
-        print(f'{index+1} is in the right order')
-    else:
-        print(f'{index+1} is not in the right order')
+def part1():
+    with open("D13input.txt") as input:
+        text = input.read()
+        pairs = [(eval(x), eval(y)) for [x, y] in [pair.split('\n') for pair in text.split('\n\n')]]
 
-print(sum)
+    sum = 0
+    for index, (left, right) in enumerate(pairs):
+        if compare_lists(left, right) == 1:
+            sum += index+1
+
+    print(sum)
+
+def part2():
+    with open("D13input.txt") as input:
+        text = input.read()
+        lines = [eval(line) for pair in text.split('\n\n') for line in pair.split('\n') ] + [[[2]], [[6]]]
+
+    compare_key = cmp_to_key(compare_lists)
+    in_order = sorted(lines, key = compare_key, reverse = True)
+
+    print((in_order.index([[2]]) + 1) * (in_order.index([[6]]) + 1))
